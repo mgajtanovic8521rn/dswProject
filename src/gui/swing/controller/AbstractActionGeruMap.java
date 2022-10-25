fcd20688113d2400c8be6c26d72fadc6b35cad9d
stevandabizljevic <sdabizljevic3920rn@raf.rs>
2022-10-25 17:57:09 +0200

    Dodate akcije u action manager iz trece nedelje

--- src/gui/swing/controller/AbstractActionGeruMap.java
+++ src/gui/swing/controller/AbstractActionGeruMap.java
@@ -2,7 +2,21 @@ package gui.swing.controller;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.net.URL;
 
 public abstract class AbstractActionGeruMap extends AbstractAction {
 
+    public Icon loadIcon(String fileName){
+
+        URL imageURL = getClass().getResource(fileName);
+        Icon icon = null;
+
+        if (imageURL != null) {
+            icon = new ImageIcon(imageURL);
+        }
+        else {
+            System.err.println("Resource not found: " + fileName);
+        }
+        return icon;
+    }
 }
---
+++ src/gui/swing/controller/NewAction.java
@@ -0,0 +1,4 @@
+package gui.swing.controller;
+
+public class NewAction {
+}
